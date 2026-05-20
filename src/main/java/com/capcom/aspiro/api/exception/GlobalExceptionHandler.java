48cc72f5d45972628de85bba179348142a4d5df8
Volha Platnitskaya <olga.plotnitskaya@gmail.com>
2026-05-20 17:40:18 +0300

    feat: add custom exceptions

--- src/main/java/com/capcom/aspiro/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/capcom/aspiro/api/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.capcom.aspiro.api.exception;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.capcom.aspiro.api.exception.custom.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
@@ -28,5 +29,41 @@ public class GlobalExceptionHandler {
                 );
 
         return errors;
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFound(
+            ResourceNotFoundException ex
+    ) {
+
+        return Map.of(
+                "error",
+                ex.getMessage()
+        );
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleBadRequest(
+            BadRequestException ex
+    ) {
+
+        return Map.of(
+                "error",
+                ex.getMessage()
+        );
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public Map<String, String> handleUnauthorized(
+            UnauthorizedException ex
+    ) {
+
+        return Map.of(
+                "error",
+                ex.getMessage()
+        );
     }
 }
